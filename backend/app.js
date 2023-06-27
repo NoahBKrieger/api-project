27cb46e6af0971fc24b0f7d3fb921dbd45658e40
noah <noahboahk@gmail.com>
2023-06-26 21:15:59 -0700

    Add Users table migration and seeds

--- backend/app.js
+++ backend/app.js
@@ -71,8 +71,6 @@ app.use((_req, _res, next) => {
 
 const { ValidationError } = require('sequelize');
 
-// ...
-
 // Process sequelize errors
 app.use((err, _req, _res, next) => {
     // check if error is a Sequelize error:
