85bc7bdb655110b7c0b8b2528a2eac1dd9c8eb23
noah <noahboahk@gmail.com>
2023-10-14 11:53:46 -0700

    navigation

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,11 @@
+export const ProfileButton = () => {
+    return (<div style={
+        {
+            color: 'purple', fontSize: "65px"
+        }
+    }
+
+    > <i className="fa-regular fa-user"></i> </div>);
+}
+
+    ;
