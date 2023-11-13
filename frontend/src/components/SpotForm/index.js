d2d8227a567d3b9e52add24c01bab8e6b6ea86b4
noah <noahboahk@gmail.com>
2023-11-12 19:37:49 -0800

    create form css

--- frontend/src/components/SpotForm/index.js
+++ frontend/src/components/SpotForm/index.js
@@ -1,5 +1,5 @@
 import { addSpotThunk } from "../../store/spotReducer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import { useHistory } from "react-router-dom";
 import { getSpotThunk } from "../../store/spotReducer";
@@ -26,11 +26,13 @@ function SpotForm() {
     const [desc, setDesc] = useState('')
     const [price, setPrice] = useState(1)
     const [errors, setErrors] = useState({});
+
     const [previewImgUrl, setPreviewImgUrl] = useState('')
     const [imgUrl1, setImgUrl1] = useState('')
     const [imgUrl2, setImgUrl2] = useState('')
     const [imgUrl3, setImgUrl3] = useState('')
     const [imgUrl4, setImgUrl4] = useState('')
+    const [prevErrors, setPrevErrors] = useState({});
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -92,6 +94,8 @@ function SpotForm() {
                     })
 
 
+
+
                     await dispatch(getSpotThunk(res.id))
                     await dispatch(getSpotReviewsThunk(res.id))
 
@@ -112,15 +116,22 @@ function SpotForm() {
         // console.log('addDis', addDis)
     }
 
+    useEffect(() => {
 
+        if (!previewImgUrl.endsWith('.png')) {
+            setPrevErrors({ errors: 'Must be a valid image url' })
+        } else { setPrevErrors({}) }
+
+    }, [previewImgUrl])
 
     return (
 
-        <>
+        <div className="form-page">
             <form className="form" onSubmit={handleSubmit}>
-                <h1 className="form-title">New Spot Form</h1>
+                <h1 className="form-title">Create a New Spot</h1>
 
                 <h2>Where's your place located?</h2>
+                <h3>Guests will only get your exact address once they booked a reservation.</h3>
                 <div className="location-div">
                     <label> Country
                         <input onChange={(e) => setCountry(e.target.value)}></input>
@@ -138,13 +149,16 @@ function SpotForm() {
                         <label> City,
                             <input onChange={(e) => setCity(e.target.value)}></input>
                         </label>
-                        {errors.city && <p>{errors.city}</p>}
+
 
                         <label> State
                             <input onChange={(e) => setState(e.target.value)}></input>
                         </label>
-                        {errors.state && <p>{errors.state}</p>}
 
+                    </div>
+                    <div className="city-state-errors">
+                        {errors.city && <p>{errors.city}</p>}
+                        {errors.state && <p className="state-errors">{errors.state}</p>}
                     </div>
 
                     <div className="lat-lng">
@@ -163,11 +177,14 @@ function SpotForm() {
                 </div>
 
                 <h2>Describe your place to guests</h2>
+                <h3>Mention the best features of your space, any special amentities like fast wifi or parking, and what you love about the neighborhood.</h3>
                 <label> Description
                     <textarea onChange={(e) => setDesc(e.target.value)}></textarea>
                 </label>
                 {errors.description && <p>{errors.description}</p>}
 
+                <h2>Create a title for your spot</h2>
+                <h3>Catch guests' attention with a spot title that highlights what makes your place special.</h3>
                 <label> Name Of Spot
                     <input onChange={(e) => setSpotName(e.target.value)}></input>
                 </label>
@@ -176,20 +193,23 @@ function SpotForm() {
                 <div className="price-div">
 
                     <h2>Set a base price for your spot</h2>
+                    <h3>Competitive pricing can help your listing stand out and rank higher in search results.</h3>
                     <label> $
-                        <input onChange={(e) => setPrice(e.target.value)}></input>
+                        <input placeholder="Price per night (USD)" onChange={(e) => setPrice(e.target.value)}></input>
                     </label>
                     {errors.price && <p>{errors.price}</p>}
 
                 </div>
 
                 <h2>Liven up your spot with photos</h2>
+                <h3>Submit a link to at least one photo to publish your spot.</h3>
 
                 <div className="add-images">
 
                     <label>Preview Image URL
                         <input onChange={(e) => setPreviewImgUrl(e.target.value)}></input>
                     </label>
+                    {prevErrors.errors && <p>{prevErrors.errors}</p>}
 
                     <label>Image URL 1
                         <input onChange={(e) => setImgUrl1(e.target.value)}></input>
@@ -214,7 +234,7 @@ function SpotForm() {
                 <button className='submit-button'>Create Spot</button>
 
             </form>
-        </>
+        </div>
     )
 
 }
